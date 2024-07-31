4c9020d9ff6f94b615722af3555bd565300362f2
Niakrist <ak_88dron@inbox.ru>
2024-07-31 14:14:51 +0300

    feat: implement adaptive layout for first page

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isBurger, setIsBurger] = useState(false);
+
+  const handleClick = () => {
+    setIsBurger((previsBurger) => !previsBurger);
+  };
+
   return (
     <header className="header">
       <div className="container header__container">
@@ -41,9 +47,49 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <a href="cart.html" className="header__cart-link">
-          6
-        </a>
+        <div className="header__btn-group">
+          <a href="cart.html" className="header__cart-link">
+            6
+          </a>
+          <button onClick={handleClick} className="burger">
+            {isBurger ? (
+              <svg
+                width="28"
+                height="28"
+                viewBox="0 0 28 28"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <rect
+                  x="7.28174"
+                  y="7.07532"
+                  width="20"
+                  height="1"
+                  transform="rotate(45 7.28174 7.07532)"
+                  fill="#D9D9D9"
+                />
+                <rect
+                  x="6.5752"
+                  y="21.2173"
+                  width="20"
+                  height="1"
+                  transform="rotate(-45 6.5752 21.2173)"
+                  fill="#D9D9D9"
+                />
+              </svg>
+            ) : (
+              <svg
+                width="28"
+                height="29"
+                viewBox="0 0 28 29"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <rect x="4" y="9.5" width="20" height="1" fill="#D9D9D9" />
+                <rect x="4" y="14.5" width="20" height="1" fill="#D9D9D9" />
+                <rect x="4" y="19.5" width="20" height="1" fill="#D9D9D9" />
+              </svg>
+            )}
+          </button>
+        </div>
       </div>
     </header>
   );
